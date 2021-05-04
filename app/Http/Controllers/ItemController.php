099cbf7342f08bb4d7bf13a43a41fa8cb5c0ee23
tayson13 <ab.naser01@gmail.com>
2021-05-04 16:00:32 +0600

    4/5 4:00pm

---
+++ app/Http/Controllers/ItemController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Item;
+use Illuminate\Support\Carbon;
+
+class ItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Item::orderBy('created_at','Desc')->get();
+        // return Item::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $item = new Item;
+        $item->name = $request->item["name"];
+        $item->save();
+
+        return $item;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $updateItem = Item::where('id', $id)->first();
+        
+        if($updateItem){
+            $updateItem->completed = $request->item["completed"] ? true : false;
+            $updateItem->completed_at = $request->item["completed_at"] ? Carbon::now() : null;
+            $updateItem->save();
+
+            return $updateItem;
+        }
+        
+        return "item not found";
+
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
